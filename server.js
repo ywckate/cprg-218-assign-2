b7e244d8da777e916cfb544fa11ff3f2a543fbfe
ywckate <kateyau238@gmail.com>
2024-04-21 23:15:30 -0600

    edit

--- server.js
+++ server.js
@@ -4,24 +4,21 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Handle form submission
 app.post("/submit-form", (req, res) => {
   const { fname, lname, phone, email } = req.body;
-  // Here, you can process the form data, e.g., save it to a database, send it via email, etc.
+
   console.log("Received form data:", { fname, lname, phone, email });
-  // Send a response back to the client
+
   res.send("Form submitted successfully!");
 });
 app.get("/success", (req, res) => {
   res.sendFile(__dirname + "/success.html");
 });
-// Serve static files (like the HTML file)
+
 app.use(express.static("public"));
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
